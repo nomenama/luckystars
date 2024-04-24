d01369b2d8734be3db8e132235bd9058a30d6935
nomen <justnomen@outlook.com>
2024-04-24 15:26:18 +0100

    restyle body

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <main className="flex flex-col items-center justify-between p-24">
+        <main className="flex-1 flex flex-col items-center justify-between p-24">
             <h1>Latest Draws</h1>
             <p>All latest draw goes here</p>
         </main>
