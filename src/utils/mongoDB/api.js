753f661b3367827ad097d26deb7abd72809d7f24
nomen <justnomen@outlook.com>
2024-04-24 15:01:09 +0100

    add dynamic rendering when utilising api call to database

--- src/utils/mongoDB/api.js
+++ src/utils/mongoDB/api.js
@@ -1,12 +1,15 @@
 import { connectToDb } from "@/utils/mongoDB/connection";
+import { unstable_noStore as noStore } from "next/cache";
 
-export async function getData(collectionName) {
+export async function getData(collectionName, limit = 6) {
+    noStore();
+
     try {
         const client = await connectToDb();
         const db = client.db();
 
         //Db query
-        return await db.collection(collectionName).find({}).toArray();
+        return await db.collection(collectionName).find({}).limit(limit).toArray();
     } catch (err) {
         console.error(err);
     }
