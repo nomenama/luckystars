53dcb48902835725164d7adbb947bc03130ab1b8
nomen <justnomen@outlook.com>
2024-04-25 17:19:41 +0100

    remove test data

--- src/utils/mongoDB/api.js
+++ src/utils/mongoDB/api.js
@@ -1,26 +1,10 @@
 import { connectToDb } from "@/utils/mongoDB/connection";
 import { unstable_noStore as noStore } from "next/cache";
-import euromillions from "@/test_data/euromillions_sorted.json";
-import lotto from "@/test_data/lotto_sorted.json";
 
 export async function getData(collectionName, limit = 20, pageNum) {
     noStore();
 
     try {
-        if (process.env.NODE_ENV === "development") {
-            const startIndex = limit * (pageNum - 1);
-            const endIndex = startIndex + limit;
-            const limitedTestData =
-                collectionName === "euromillions"
-                    ? euromillions.slice(startIndex, endIndex)
-                    : lotto.slice(startIndex, endIndex);
-
-            return {
-                draws: limitedTestData,
-                count: collectionName === "euromillions" ? euromillions.length : lotto.length,
-            };
-        }
-
         const client = await connectToDb();
         const db = client.db();
 
