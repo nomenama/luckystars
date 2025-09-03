ee37cd51d31b7ea43d8b2a6ee5b072b124c557cc
Nomen Ama <justnomen@outlook.com>
2025-09-03 10:37:25 +0100

    add loading indicator between pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/app/components/Header";
 import { Footer } from "@/app/components/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,17 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
+                <NextTopLoader
+                    color="#117aec"
+                    initialPosition={0.08}
+                    crawlSpeed={200}
+                    height={3}
+                    crawl={true}
+                    showSpinner={true}
+                    easing="ease"
+                    speed={200}
+                    shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+                />
                 <link rel="icon" href="/logo.png" />
                 <Header />
                 {children}
